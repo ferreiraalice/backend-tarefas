be0866c354037cc384cd042f013ea92ede590064
ferreiraalice <alice.ferreira.barbosa@escola.pr.gov.br>
2026-03-10 08:39:33 -0300

    Add files via upload

---
+++ stringo.js
@@ -0,0 +1,12 @@
+console.log("ENDEREÇO COMPLETO");
+
+let rua = "Rua das flores";
+let numero = 9851;
+let cidade = "Curitiba";
+let cep = "80010-000";
+
+let enderecoCompleto = rua + ", " + numero + " - " + cep;
+
+console.log(enderecoCompleto);
+
+//Adicione informações adicionais
